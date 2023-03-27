402ed7028c34332bfe0e751ee72559025d21e7e6
BodeWijnands <63235778+BodeWijnands@users.noreply.github.com>
2023-03-27 20:52:25 +0200

    works with custom username password
    -add volume mapping
    - not accessible outside of network

--- expressApi/services/database.js
+++ expressApi/services/database.js
@@ -5,7 +5,6 @@ const { MongoClient } = require("mongodb");
 const uri = process.env.MONGO_URL;
 console.log("url is: ", uri)
 const client = new MongoClient(uri, {
-
     useNewUrlParser: true,
     useUnifiedTopology: true,
     family: 4
@@ -14,18 +13,12 @@ const client = new MongoClient(uri, {
 let dbConnection;
 
 module.exports = {
-
     client: client,
-
     getDb: async function () {
-
         if (!dbConnection) {
-
             await client.connect();
-
             dbConnection = client.db(process.env.DB_NAME);
         }
-
         return dbConnection;
     }
 };
