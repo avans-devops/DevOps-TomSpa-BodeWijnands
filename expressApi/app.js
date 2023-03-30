29bdfc15a859065ab3b429e05483d648d1f65fc8
BodeWijnands <63235778+BodeWijnands@users.noreply.github.com>
2023-03-30 14:02:04 +0200

    Added mongo exporter & grafana dashboards

--- expressApi/app.js
+++ expressApi/app.js
@@ -4,11 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
 const promBundle = require('express-prom-bundle');
 const metricsMiddleware = promBundle({
 	includePath: true,
@@ -19,9 +14,12 @@ const metricsMiddleware = promBundle({
 	}
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-app.use(metricsMiddleware); // enable Metrics middleware
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -32,6 +30,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(metricsMiddleware); // enable Metrics middleware
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
